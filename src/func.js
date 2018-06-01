bade9d3a060957d0d6056171956a777749d4069f
Saeki, Takahiro <ts-takahiro.saeki@rakuten.com>
2018-06-01 16:33:10 +0900

    update: test

--- src/func.js
+++ src/func.js
@@ -1,4 +1,5 @@
 import name from './name.js';
+import _ from 'lodash';
 
 const render = `${name} hoge`;
 
--- vunder.config.js
+++ vunder.config.js
@@ -2,10 +2,11 @@ module.exports = {
   entry: 'src/index.js',
   output: 'dist/app.bundle.min.js',
   minify: false,
+  extension: '.js',
   server: {
     root: './dist',
     port: 8080,
-    open: true
+    open: false
   },
   template: {
     format: 'ejs',
